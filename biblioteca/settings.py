209034b26ff9199db395d75e65e30a5605acfca3
Andres <andrecraft65@gmail.com>
2026-01-13 19:37:36 -0600

    first commit

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h971@swhg!v#%(45@k($4(e9m3g2ytv)o0ga2hk#t(jq$sj80t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -116,3 +116,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
